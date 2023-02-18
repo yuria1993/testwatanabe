75f1884fda0b4974f700bff290aed0d2c45491eb
wadakatu <wadakatukoyo330@gmail.com>
2023-02-19 03:09:18 +0900

    サンクスページ修正

--- resources/views/send.blade.php
+++ resources/views/send.blade.php
@@ -1,12 +1,11 @@
 <x-guest-layout>
 
-  <p class="text-center text-[24px] py-5">ご覧いただきありがとうございました。</p>
+    <p class="text-center text-[24px] py-5">ご覧いただきありがとうございました。</p>
 
-  <div class="flex justify-center py-5">
-    <form method="post" action="">
-      @csrf
-      <button type="submit" class="text-center bg-black border-4 border-black text-white rounded-md">トップページへ</button>
-    </form>
-  </div>
+    <div class="flex justify-center py-5">
+        <button type="submit" class="text-center bg-black border-4 border-black text-white rounded-md">
+            <a href="{{route('form')}}">トップページへ</a>
+        </button>
+    </div>
 
 </x-guest-layout>
