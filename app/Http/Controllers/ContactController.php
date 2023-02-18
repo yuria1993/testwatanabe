2069a7f5ffe374e648164ef6ebc675729b757247
wadakatu <wadakatukoyo330@gmail.com>
2023-02-19 03:06:31 +0900

    コントローラーとリクエストクラス修正

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers;
 
 use Carbon\CarbonImmutable;
-use App\Http\Requests\TestRequest;
+use Illuminate\Support\Arr;
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 use App\Models\Contact;
 use Illuminate\Database\Eloquent\Builder;
 
-class TestsController extends Controller
+class ContactController extends Controller
 {
     public function index()
     {
         return view('index');
     }
 
-    public function confirm(TestRequest $request)
+    public function confirm(ContactRequest $request)
     {
         $contents = $request->all();
         return view('confirm', ['contents' => $contents]);
@@ -35,18 +36,19 @@ class TestsController extends Controller
     {
         $searchParams = $request->all();
 
+
         $contents = Contact::query()
             ->when(!empty($searchParams), function (Builder $builder) use ($searchParams) {
                 $builder->when(
-                    !is_null($searchParams['fullname']),
+                    !is_null(Arr::get($searchParams, 'fullname')),
                     fn(Builder $builder) => $builder->where('fullname', 'LIKE', "%" . $searchParams['fullname'] . "%")
                 );
                 $builder->when(
-                    !is_null($searchParams['gender']),
+                    !is_null(Arr::get($searchParams, 'gender')),
                     fn(Builder $builder) => $builder->where('gender', $searchParams['gender'])
                 );
                 $builder->when(
-                    !is_null($searchParams['created_at_from']),
+                    !is_null(Arr::get($searchParams, 'created_at_from')),
                     fn(Builder $builder) => $builder->where(
                         'created_at',
                         '>=',
@@ -56,18 +58,18 @@ class TestsController extends Controller
                     )
                 );
                 $builder->when(
-                    !is_null($searchParams['created_at_to']),
+                    !is_null(Arr::get($searchParams, 'created_at_to')),
                     fn(Builder $builder) => $builder->where(
                         'created_at',
                         '<=',
                         CarbonImmutable::parse(
                             $searchParams['created_at_to']
-                        )->startOfDay()->toDateTimeString()
+                        )->endOfDay()->toDateTimeString()
                     )
                 );
                 $builder->when(
-                    !is_null($searchParams['email']),
-                    fn(Builder $builder) => $builder->where('email', $searchParams['email'])
+                    !is_null(Arr::get($searchParams, 'email')),
+                    fn(Builder $builder) => $builder->where('email', 'LIKE', '%' . $searchParams['email'] . '%')
                 );
             })
             ->paginate(10);
